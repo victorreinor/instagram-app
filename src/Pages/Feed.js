7640025c3c5ab14f8023a331b90f55dd506f759b
Victor Reinor <victorreinor@gmail.com>
2019-06-17 21:32:54 -0300

    adiciona o arquivo feed e sua configuracao inicial

---
+++ src/Pages/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
