65abd05000e89acf3ef11d81dfa2373262d3f008
Victor Reinor <victorreinor@gmail.com>
2019-06-17 21:32:28 -0300

    altera as configuracoes de app para index e deixa app somente como ponto de entreda

--- App.js
+++ App.js
@@ -1,26 +1,4 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import App from './src'
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text styles={styles.welcome}>Welcome to Rocketseat!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
+export default () => <App />
