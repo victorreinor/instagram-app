d773567bf48233870532a9d823f2913fa615852d
Victor Reinor <victorreinor@gmail.com>
2019-06-19 12:40:48 -0300

    adicionar a estilizacao e a estrutura da tela de criar postage

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -1,9 +1,144 @@
 import React, { Component } from "react";
 
-import { View } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  Image
+} from "react-native";
 
 export default class New extends Component {
+  static navigationOptions = {
+    headerTitle: "Nova publicação"
+  };
+
+  state = {
+    author: "",
+    place: "",
+    description: "",
+    hashtags: ""
+  };
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.selectButton} onPress={() => false}>
+          <Text style={styles.selectButtonText}>Selecionar imagem</Text>
+        </TouchableOpacity>
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome do autor"
+          placeholderTextColor="#999"
+          value={this.state.author}
+          onChangeText={author => this.setState({ author })}
+        />
+
+        <TextInput
+          style={styles.inp  ut}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Local da foto"
+          placeholderTextColor="#999"
+          value={this.state.place}
+          onChangeText={place => this.setState({ place })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome do autor"
+          placeholderTextColor="#999"
+          value={this.state.author}
+          onChangeText={author => this.setState({ author })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Descrição"
+          placeholderTextColor="#999"
+          value={this.state.description}
+          onChangeText={description => this.setState({ description })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Hashtags"
+          placeholderTextColor="#999"
+          value={this.state.hashtags}
+          onChangeText={hashtags => this.setState({ hashtags })}
+        />
+
+        <TouchableOpacity style={styles.shareButton} onPress={() => false}>
+          <Text style={styles.shareButtonText}>Compartilhar</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 30
+  },
+
+  selectButton: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#CCC",
+    borderStyle: "dashed",
+    height: 42,
+
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  selectButtonText: {
+    fontSize: 16,
+    color: "#666"
+  },
+
+  preview: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+    alignSelf: "center",
+    borderRadius: 4
+  },
+
+  input: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    padding: 15,
+    marginTop: 10,
+    fontSize: 16
+  },
+
+  shareButton: {
+    backgroundColor: "#7159c1",
+    borderRadius: 4,
+    height: 42,
+    marginTop: 15,
+
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  shareButtonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#FFF"
+  }
+});
