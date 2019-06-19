92eb895fa088631f88a66f86b63cf6d770b476f4
Victor Reinor <victorreinor@gmail.com>
2019-06-18 21:18:43 -0300

    altera o nome da pasta pages e fazer retornar uma linha so para ficar no padrao o codigo

--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { View } from 'react-native';
+import { View } from "react-native";
 
 export default class Feed extends Component {
   render() {
-    return (
-      <View>
-
-      </View>
-    )
+    return <View />;
   }
 }
