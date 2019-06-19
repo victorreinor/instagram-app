6670f31a9d845eaef2afb67411359cb202c18f38
Victor Reinor <victorreinor@gmail.com>
2019-06-19 12:41:02 -0300

    adicionar socket-io para requisicoes em tempo real

--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../services/api";
+import io from "socket.io-client";
 
 import {
   View,
@@ -33,12 +34,32 @@ export default class Feed extends Component {
   };
 
   async componentDidMount() {
-    // this.registerToSocket();
+    this.registerToSocket();
 
     const response = await api.get("posts");
 
     this.setState({ feed: response.data });
   }
+
+  registerToSocket = () => {
+    const socket = io("http://192.168.0.186:3333");
+
+    socket.on("post", newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on("like", likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`);
+  };
 
   render() {
     return (
@@ -59,13 +80,16 @@ export default class Feed extends Component {
               <Image
                 style={styles.feedImage}
                 source={{
-                  uri: `http://192.168.0.103:3333/files/${item.image}`
+                  uri: `http://192.168.0.186:3333/files/${item.image}`
                 }}
               />
 
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity style={styles.action} onPress={() => false}>
+                  <TouchableOpacity
+                    style={styles.action}
+                    onPress={() => this.handleLike(item._id)}
+                  >
                     <Image source={like} />
                   </TouchableOpacity>
 
