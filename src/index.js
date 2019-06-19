c0df0f8476eae8c9649c059c73500e6f8a4a60f7
Victor Reinor <victorreinor@gmail.com>
2019-06-18 21:17:59 -0300

    altera para arrow function o component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
-import Routes from './routes';
+import Routes from "./routes";
 
-export default function Index() {
-  return <Routes />
-}
+export default () => <Routes />;
